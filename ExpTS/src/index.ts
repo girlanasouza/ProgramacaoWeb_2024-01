afeba6b26a1b931bb6d782eba4a5ad10b61e3ace
girlanasouza <girlanasouza123@gmail.com>
2024-06-30 09:05:56 -0400

    exercicio 3 - ExpTS

--- ExpTS/src/index.ts
+++ ExpTS/src/index.ts
@@ -1,11 +1,36 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
+import fs from "fs";
+import path from "path";
+
 dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 3333;
+const logFolder = process.env.LOG_FOLDER || "logs";
+const logFormat = process.env.LOG_FORMAT;
+
+app.use((req: Request, res: Response, next) => {
+  const now = new Date().toISOString();
+  const logFilePath = path.join(logFolder, "access.log");
+
+  let logMessage;
+
+  if (logFormat === "completo") {
+    logMessage = `${now} ${req.method} ${req.url} HTTP/${req.httpVersion} ${req.headers["user-agent"]}\n`;
+  } else {
+    logMessage = `${now} ${req.method} ${req.url}\n`;
+  }
+
+  fs.appendFileSync(logFilePath, logMessage, "utf8");
+
+  next();
+});
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello world!");
 });
+
 app.listen(PORT, () => {
   console.log(`Express app iniciada na porta ${PORT}.`);
 });
